b26b0e7e1c0599efdc8d65ca2a5a50aa268a6061
Miguel <m.lapchinskiy@gmail.com>
2026-03-06 12:31:21 +0300

    "Fix: remove node_modules from repository"

---
+++ frontend/src/api/index.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: "http://localhost:3000/api",
+    headers: {
+        "Content-Type": "application/json",
+        "accept": "application/json"
+    }
+});
+
+export const api = {
+    getUsers: async () => {
+        const response = await apiClient.get("/users");
+        return response.data;
+    },
+    getUserById: async (id) => {
+        const response = await apiClient.get(`/users/${id}`);
+        return response.data;
+    },
+    createUser: async (user) => {
+        const response = await apiClient.post("/users", user);
+        return response.data;
+    },
+    updateUser: async (id, user) => {
+        const response = await apiClient.patch(`/users/${id}`, user);
+        return response.data;
+    },
+    deleteUser: async (id) => {
+        await apiClient.delete(`/users/${id}`);
+    }
+};
